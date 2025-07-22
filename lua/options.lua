5c030b21c9a8c600a86b9ff53b11c04ad9581845
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-07-23 02:42:18 +0500

    options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,68 @@
-require "nvchad.options"
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
 
--- add yours here!
+-------------------------------------- options ------------------------------------------
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+o.ruler = true
+o.colorcolumn = "100"
+o.wrap = false
+
+o.hlsearch = false
+o.incsearch = true
+
+o.termguicolors = true
+
+o.laststatus = 3
+o.showmode = false
+o.splitkeep = "screen"
+
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.cursorlineopt = "number"
+
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 4
+o.smartindent = true
+o.tabstop = 4
+o.softtabstop = 4
+
+opt.fillchars = { eob = " " }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+
+-- Numbers
+o.number = true
+o.numberwidth = 2
+o.rnu = true
+o.scrolloff = 15
+
+-- disable nvim intro
+opt.shortmess:append "sI"
+
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+o.undofile = true
+
+-- interval for writing swap file to disk, also used by gitsigns
+o.updatetime = 50
+
+-- go to previous/next line with h,l,left arrow and right arrow
+-- when cursor reaches end/beginning of line
+opt.whichwrap:append "<>[]hl"
+
+-- disable some default providers
+g.loaded_node_provider = 0
+g.loaded_python3_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_ruby_provider = 0
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.fn.has "win32" ~= 0
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
