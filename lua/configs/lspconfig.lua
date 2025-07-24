b0a2c219e76473896b33e151dcb3b87c1a1ff55c
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-07-24 23:58:41 +0500

    now code completion and suggestions are working

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,16 @@
-require("nvchad.configs.lspconfig").defaults()
+local config = require("nvchad.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
 
-local servers = { "html", "cssls", "typescript", "tailwindcss" }
-vim.lsp.enable(servers)
+local lspconfig = require("lspconfig")
 
+lspconfig.tsserver.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    init_options = {
+        preferences = {
+            disableSuggestions = true,
+        }
+    }
+}
 -- read :h vim.lsp.config for changing options of lsp servers 
