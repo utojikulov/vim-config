d1dd592da6bef26ab9f4776105744e10ae6ae591
Ulugbek Tojikulov <cookie.pechehbkov@mail.ru>
2025-07-24 23:58:12 +0500

    made the dotfiles visible in nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,15 +1,20 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      view = {
-        width = 30,
-        side = "right",
-      },
-      renderer = {
-        highlight_git = true,
-        highlight_opened_files = "all",
-      },
+    {
+        "nvim-tree/nvim-tree.lua",
+        opts = {
+            view = {
+                width = 30,
+                side = "right",
+            },
+            renderer = {
+                highlight_git = true,
+                highlight_opened_files = "all",
+            },
+            git = {
+                enable = true,
+                ignore = false,
+                timeout = 500,
+            } 
+        },
     },
-  },
 }
